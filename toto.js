ad3c8028b385601dd84e4286ff0aa1ca8f5e80ea
Quentin <you@example.com>
2023-08-04 18:58:53 +0200

    init project

---
+++ toto.js
@@ -0,0 +1,7 @@
+var prenom = 'quentin';
+
+// pow
+
+// git config --global user.name "Quentin"
+// git config --global user.mail "quentinpetit09@gmail.com"
+// repo init -u https://github.com/QuentinPetit31/test.git
